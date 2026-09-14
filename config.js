5a5b647b951518253a039dce14789549c2db8108
Shikha Prajapati <shikha992003@gmail.com>
2026-09-14 13:24:59 +0530

    [fix]: include config.js for GitHub Pages live supabase connection

---
+++ config.js
@@ -0,0 +1,6 @@
+// Local Environment Configuration (Ignored by Git)
+window.__ENV__ = {
+  SUPABASE_URL: "https://wurmheablncfwupvmbbm.supabase.co",
+  SUPABASE_KEY: "sb_publishable__6dWW9Fcs3uexlGTuMG0Fw_ga1t5jr-"
+};
+
